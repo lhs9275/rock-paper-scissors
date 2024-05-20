0a369b0d515634ade5934a9838174052c5b65e3c
my <hslee9275@gmail.com>
2024-05-20 17:55:51 +0900

    commit

--- public/mjp.js
+++ public/mjp.js
@@ -1,47 +1,57 @@
+let m = "주먹";
+let j = "가위";
+let p = "보";
+
+var bot = "bot"; 
+
 
 function rock(){
     let lee = Math.floor(Math.random()*3);
-    if (lee == 1){
-        alert("Bot:가위\n  마이턴~");
-        location.replace("MJP2");
-
+    if (lee == 0){
+        alert(`Bot: ${m} \n 다시~`);
         }
-    else if (lee == 0){
-        alert("Bot:주먹\n 다시")
+    else if (lee == 1){
+        alert(`Bot: ${j}\n 마이턴`)
         time()
     }
     else if (lee == 2){
-        alert("Bot:보\n 피씨턴~ ")
+        alert(`Bot:${p}\n 피씨턴~`)
         time()
 
     }
-}
+};
 function si(){
     let lee = Math.floor(Math.random()*3);
-    if (lee == 1){
-        alert("Bot:주먹\n 다시")
-    }
-    else if (lee == 0){
-        alert("Bot:보\n 피씨턴~ ")
+    if (lee == 0){
+        alert(`Bot: ${m} \n 피씨턴`);
+        location.replace("MJP2");
+        }
+    else if (lee == 1){
+        alert(`Bot: ${j}\n 다시`)
+        time()
     }
     else if (lee == 2){
-        alert("Bot:가위\n  마이턴~");
+        alert(`Bot:${p}\n  마이턴`)
+        time()
+
     }
-}
+};
+
 function paeper(){
     let lee = Math.floor(Math.random()*3);
-    if (lee == 1){
-        alert("Bot:보\n 피씨턴~ ")
-    }
-    else if (lee == 0){
-        alert("Bot:가위\n  마이턴~");
-    }
+    if (lee == 0){
+        alert(`Bot: ${m} \n  마이턴~`);
+            }
+    else if (lee == 1){
+        alert(`Bot: ${j}\n 피씨턴`)
+        time()
+        }
     else if (lee == 2){
-        alert("Bot:주먹\n 다시")
+        alert(`Bot:${p}\n 다시~`)
+        time()
+    
     }
-}
-
-
+};
 function time(){  
 
 }
