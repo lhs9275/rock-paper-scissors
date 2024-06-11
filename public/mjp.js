de6d790f09b6438f2bcee821adf1ce9fd38b94c6
my <hslee9275@gmail.com>
2024-06-11 17:21:47 +0900

    묵찌빠 완성

--- public/mjp.js
+++ public/mjp.js
@@ -1,57 +1,84 @@
-let m = "주먹";
-let j = "가위";
-let p = "보";
-
-var bot = "bot"; 
+let m = '묵';
+let j = '찌';
+let p ='빠';
 
 
 function rock(){
     let lee = Math.floor(Math.random()*3);
+    let result;
     if (lee == 0){
-        alert(`Bot: ${m} \n 다시~`);
-        }
+        result =`Bot: ${m} \n 다시~`;
+    }
     else if (lee == 1){
-        alert(`Bot: ${j}\n 마이턴`)
-        time()
+        result =`Bot: ${j}\n 마이턴`;
     }
     else if (lee == 2){
-        alert(`Bot:${p}\n 피씨턴~`)
-        time()
-
+        result =`Bot:${p}\n 피씨턴~`;
     }
+
+alert(result);
+
+$.ajax({
+    url: '/updateResult',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({ result: result }),
+    success: function() {
+        window.location.replace("/MJP2");
+    }
+});
+
 };
+
 function si(){
     let lee = Math.floor(Math.random()*3);
+    let result;
     if (lee == 0){
-        alert(`Bot: ${m} \n 피씨턴`);
-        location.replace("MJP2");
-        }
+        result = `Bot: ${m} \n 피씨턴`;
+    }
     else if (lee == 1){
-        alert(`Bot: ${j}\n 다시`)
-        time()
+        result = `Bot: ${j}\n 다시`;
     }
     else if (lee == 2){
-        alert(`Bot:${p}\n  마이턴`)
-        time()
-
+        result = `Bot:${p}\n  마이턴`;
     }
+
+    alert(result);
+    
+    $.ajax({
+        url: '/updateResult',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ result: result }),
+        success: function() {
+            window.location.replace("/MJP2");
+        }
+    });
+
 };
 
-function paeper(){
+function pa(){
+    letresult;
     let lee = Math.floor(Math.random()*3);
     if (lee == 0){
-        alert(`Bot: ${m} \n  마이턴~`);
-            }
-    else if (lee == 1){
-        alert(`Bot: ${j}\n 피씨턴`)
-        time()
-        }
-    else if (lee == 2){
-        alert(`Bot:${p}\n 다시~`)
-        time()
-    
+        result=`Bot: ${m} \n  마이턴~`;
     }
-};
-function time(){  
+    else if (lee == 1){
+        result =`Bot: ${j}\n 피씨턴`;
+    }
+    else if (lee == 2){
+        result = `Bot:${p}\n 다시~`;
+    }
+    
+    alert(result);
 
-}
+    $.ajax({
+        url: '/updateResult',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ result: result }),
+        success: function() {
+            window.location.replace("/MJP2");
+        }
+    });
+};
--- sever.js
+++ sever.js
@@ -1,24 +1,36 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var bot_result_viewer = { result: "결과값" };
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use(express.static("img"));
+app.use(express.static('img'));
 
-app.get('/',function(req,res){
-  res.render('main.ejs')
-})
+app.get('/', function(req, res) {
+    res.render('main');
+});
 
-app.get('/RSP',function(req,res){
-  res.render('rocksipaper.ejs')
-})
+app.get('/RSP', function(req, res) {
+    res.render('rocksipaper');
+});
 
-app.get('/MJP',function(req,res){
-  res.render('mookjibba.ejs')
-})
+app.get('/MJP', function(req, res) {
+    res.render('mookjibba');
+});
 
-app.get('/MJP2',function(req,res){
-   res.render('mjp2.ejs')
-})
-app.listen(3000);
+app.post('/updateResult', function(req, res) {
+    bot_result_viewer.result = req.body.result;
+    res.sendStatus(200);
+});
+
+app.get('/MJP2', function(req, res) {
+    res.render('mjp2', bot_result_viewer);
+});
+
+app.listen(3000, function() {
+    console.log('서버가 3000번 포트에서 실행 중입니다');
+});
